00969cda9a6d6e1050c95e16e5ee0fe6176a6d12
CollieIsCute <qetup1988@gmail.com>
2021-12-13 03:25:37 +0800

    remove unused attribution in phi class

--- programs/philosopher.hpp
+++ programs/philosopher.hpp
@@ -13,12 +13,11 @@
 
 class Philosopher {
 	int ID;
-	int mealCnt;
 	std::string getSignature() const;
 
 public:
 	Philosopher() = delete;
-	Philosopher(int ID) : ID{ ID }, mealCnt{ 0 } {}
+	Philosopher(int ID) : ID{ ID } {}
 	//定義第 ID 根筷子為 left_chop, ID+1 為 right_chop, 若 ID+1 overflow, 則第 0 為 right_chop
 	void eat(std::vector<Chopsticks>& chop, const bool leftFirst = rand() % 2) const;
 	void think() const;
